a9b30dda8fd8ef13ab094682e87d44a8697af3d2
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-17 22:54:05 -0500

    Adding actions to appbar and make fadeinimage to appear in body

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://cdn.britannica.com/73/137473-050-EEF6EAFF/Stan-Lee.jpg'),
+              radius: 23.0,
+            ),
+          ),
+          CircleAvatar(
+            child: Text('SL'),
+            backgroundColor: Colors.brown,
+          ),
+          SizedBox(
+            width: 8,
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://cdn.britannica.com/73/137473-050-EEF6EAFF/Stan-Lee.jpg'),
+          placeholder: AssetImage('assets/loading.gif'),
+          fadeInDuration: Duration(
+            milliseconds: 2000
+          ),
+        ),
       ),
     );
   }
