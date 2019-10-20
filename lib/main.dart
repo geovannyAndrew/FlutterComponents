01fdd8ae1410516075f3215488b0c8ae04109383
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-10-20 00:18:11 -0500

    Creating a DatePicker and importing internationalization languages to datepicker.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/routes/routes.dart';
 import 'package:flutter_widgets/src/pages/alert_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
  
 void main() => runApp(MyApp());
  
@@ -8,6 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en','US'), // English
+        const Locale('es','ES'), // Spanish
+      ],
       title: 'Components App',
       debugShowCheckedModeBanner: false,
       routes: getApplicationRoutes(),
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,6 +10,8 @@ class _InputPageState extends State<InputPage> {
 
   String _name = '';
   String _email = '';
+  String _date = '';
+  TextEditingController _inputFieldDateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +27,8 @@ class _InputPageState extends State<InputPage> {
            _createEmail(),
            Divider(),
            _createPassword(),
+           Divider(),
+           _createDate(context),
            Divider(),
            _createPerson(),
            Divider(),
@@ -136,9 +140,57 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+  Widget _createDate(BuildContext context) {
+    return TextField(
+      onTap: (){
+        FocusScope.of(context).requestFocus(FocusNode());
+        _selectDate(context);
+      },
+      controller: _inputFieldDateController,
+      enableInteractiveSelection: false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.yellow,
+            width: 3
+          )
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.yellow,
+            width: 3
+          )
+        ),
+        counter: Text('Letras ${_email.length}'),
+        counterStyle: TextStyle(
+          color: Colors.purple
+        ),
+        hintText: 'Birthday',
+        labelText: 'Birthday',
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today)
+      ),
+    );
+  }
+
   Widget _createPerson() {
     return ListTile(
       title: Text(_name),
     );
+  }
+
+  _selectDate(BuildContext context) async{
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2018),
+      lastDate: DateTime(2025),
+      locale: Locale('es','ES')
+    );
+
+    if(picked != null){
+      _date = picked.toString();
+      _inputFieldDateController.text = _date;
+    }
   }
 }
